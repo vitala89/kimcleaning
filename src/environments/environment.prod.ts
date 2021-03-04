0c4b6d6e025e66090c5428e20566a024d3d5395f
Vitalii <60993299+vitala89@users.noreply.github.com>
2021-03-04 22:10:42 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiKey: "AIzaSyDv6ksrzasnunEK-0Rp88ajX-FqY4XJww09",
-  fbDbUrl: "https://kimcleaning-default-rtdb.firebaseio.com/"
+  apiKey: "",
+  fbDbUrl: ""
 };
